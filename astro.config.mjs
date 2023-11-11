7b255b113d40e5906f29e6ea9caa5b57223d0f12
jun16o <jun16o.shimada@gmail.com>
2023-11-11 23:06:13 +0900

    add deploy settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://jun16o.github.io',
+  base: '/dev-starlight01',
 	integrations: [
 		starlight({
 			title: 'サービス名 | ヘルプセンター',
